9b742dcaf3825d1851c1eff9040c9db09306b321
liutaihua <defage@gmail.com>
2016-02-17 16:53:03 +0800

    improve html struct

--- static/js/scroll-load.js
+++ static/js/scroll-load.js
@@ -1,56 +1,16 @@
-var scrollLoad = (function (options) {
-    var defaults = (arguments.length == 0) ? {src: 'xSrc', time: 300} : {
-        src: options.src || 'xSrc',
-        time: options.time || 300
-    };
-    var camelize = function (s) {
-        return s.replace(/-(\w)/g, function (strMatch, p1) {
-            return p1.toUpperCase();
+$(function () {
+    $(function () {
+        $("img.lazy").lazyload({
+//                    event: "sporty",
+            effect: "fadeIn",
+            effectTime: 200,
+            threshold: 200
         });
-    };
-    this.getStyle = function (element, property) {
-        if (arguments.length != 2) return false;
-        var value = element.style[camelize(property)];
-        if (!value) {
-            if (document.defaultView && document.defaultView.getComputedStyle) {
-                var css = document.defaultView.getComputedStyle(element, null);
-                value = css ? css.getPropertyValue(property) : null;
-            } else if (element.currentStyle) {
-                value = element.currentStyle[camelize(property)];
-            }
-        }
-        return value == 'auto' ? '' : value;
-    };
-    var _init = function () {
-        var offsetPage = window.pageYOffset ? window.pageYOffset : window.document.documentElement.scrollTop,
-            offsetWindow = offsetPage + Number(window.innerHeight ? window.innerHeight : document.documentElement.clientHeight),
-            docImg = document.images,
-            _len = docImg.length;
-        if (!_len) return false;
-        for (var i = 0; i < _len; i++) {
-            var attrSrc = docImg[i].getAttribute(defaults.src),
-                o = docImg[i], tag = o.nodeName.toLowerCase();
-            if (o) {
-                postPage = o.getBoundingClientRect().top + window.document.documentElement.scrollTop + window.document.body.scrollTop;
-                postWindow = postPage + Number(this.getStyle(o, 'height').replace('px', ''));
-                if ((postPage > offsetPage && postPage < offsetWindow) || (postWindow > offsetPage && postWindow < offsetWindow)) {
-                    // if (tag === "img" && attrSrc !== null) {
-                    //     o.setAttribute("src", attrSrc);
-                    // }
-                    if (attrSrc !== null) {
-                        o.setAttribute("src", attrSrc);
-                    }
-                    o = null;
-                }
-            }
-        }
-        ;
-        window.onscroll = function () {
-            setTimeout(function () {
-                _init();
-            }, defaults.time);
-        }
-    };
-    return _init();
+    });
+    $(window).bind("load", function () {
+        var timeout = setTimeout(function () {
+            $("img.lazy").trigger("sporty")
+        }, 5000);
+    });
 });
-scrollLoad();
+
